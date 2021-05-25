b6dc768b52d89cd88cfa0fd4e9879456e755e363
Erik Borg <e.borg1990@gmail.com>
2021-05-25 23:24:41 +0300

    update map no rerender

--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
-import React, { useState, useEffect } from "react";
-import { render } from "react-dom";
-import "./style.css";
-import useMapbox from "./useMapbox.js";
+import React, { useState, useEffect, useRef } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+import useMapbox from './useMapbox.js';
 
 function App() {
-    const [currentShop, setCurrentShop] = useState("km20");
-    const [marker, setCurrentMarker] = useState([37.610641, 55.761994]);
+  const map = useRef(null);
+  const [currentShop, setCurrentShop] = useState('km20');
+  const [marker, setCurrentMarker] = useState([37.610641, 55.761994]);
 
-    useEffect(() => {
-        if (currentShop === "km20") {
-            setCurrentMarker([37.610641, 55.761994]);
-        }
-        if (currentShop === "belief") {
-            setCurrentMarker([37.601152, 55.733396]);
-        }
-        if (currentShop === "brandshop") {
-            setCurrentMarker([37.616812, 55.767839]);
-        }
-    }, [currentShop]);
+  useEffect(() => {
+    if (currentShop === 'km20') {
+      setCurrentMarker([37.610641, 55.761994]);
+    }
+    if (currentShop === 'belief') {
+      setCurrentMarker([37.601152, 55.733396]);
+    }
+    if (currentShop === 'brandshop') {
+      setCurrentMarker([37.616812, 55.767839]);
+    }
+  }, [currentShop]);
 
-    useMapbox(marker);
-    return (
-        <>
-            <div className="map-overlay">
-                <h3>Выберите магазин: </h3>
-                <select
-                    onChange={(e) => {
-                        setCurrentShop(e.target.value);
-                    }}
-                >
-                    <option value="km20">KM20</option>
-                    <option value="belief">BELIEF</option>
-                    <option value="brandshop">BRANDSHOP</option>
-                </select>
-            </div>
-            <div id="map"></div>
-        </>
-    );
+  useMapbox(map, 'map', marker, 12);
+  return (
+    <>
+      <div className="map-overlay">
+        <h3>Выберите магазин: </h3>
+        <select
+          onChange={(e) => {
+            setCurrentShop(e.target.value);
+          }}
+        >
+          <option value="km20">KM20</option>
+          <option value="belief">BELIEF</option>
+          <option value="brandshop">BRANDSHOP</option>
+        </select>
+      </div>
+      <div id="map"></div>
+    </>
+  );
 }
 
-render(<App />, document.querySelector("#root"));
+render(<App />, document.querySelector('#root'));
--- src/useMapbox.js
+++ src/useMapbox.js
@@ -1,16 +1,19 @@
-import mapboxgl from '!mapbox-gl';
-import { useEffect } from "react";
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import { useEffect } from 'react';
 
-export default function useMapbox(marker = [37.610641, 55.761994]) {
-    mapboxgl.accessToken =
-        "pk.eyJ1IjoiZXJib3JnIiwiYSI6ImNrb2Q0b21nZDFob3oyb3FtdHNjNzgxbGUifQ.hEsH0NC3rOAEIXAtg14rwA";
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-            container: "map",
-            style: "mapbox://styles/mapbox/dark-v10",
-            center: [37.610641, 55.761994],
-            zoom: 12
-        });
-        new mapboxgl.Marker().setLngLat(marker).addTo(map);
+export default function useMapbox(map, container, marker = [37.610641, 55.761994], zoom) {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiZXJib3JnIiwiYSI6ImNrb2Q0b21nZDFob3oyb3FtdHNjNzgxbGUifQ.hEsH0NC3rOAEIXAtg14rwA';
+  useEffect(() => {
+    if (map.current) return;
+    map.current = new mapboxgl.Map({
+      container: container,
+      style: 'mapbox://styles/mapbox/dark-v10',
+      center: marker,
+      zoom: zoom,
     });
+  });
+  useEffect(() => {
+    const mrk = new mapboxgl.Marker().setLngLat(marker).addTo(map.current);
+    return () => mrk.remove();
+  });
 }
